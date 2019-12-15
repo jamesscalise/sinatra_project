a302b6255c5c8aeb57ea50374f347eeb38f2e40f
James Scalise <jamescscalise@gmail.com>
2019-12-15 18:03:38 +0000

    blah

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < App
+  
+  get "/signup" do
+  end
+  
+  post "/signup" do
+  end
+  
+  get "/login" do
+  end
+  
+  post "/login" do
+  end
+  
+  get "/logout" do
+  end
+  
+  get "/users/:slug" do
+  end
 
 end
