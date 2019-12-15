6e09e9ca7d0a2003edae013d2a6d918d1de90a80
James Scalise <jamescscalise@gmail.com>
2019-12-15 18:10:33 +0000

    controller outlines

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,26 @@
 class ItemsController < App
+  
+  get '/items' do
+  end
+  
+  get '/items/new' do
+  end
+  
+  post '/items'
+  end
+  
+  get '/items/:id' do
+  end
+
+
+  get '/items/:id/edit' do
+    
+  end
+  
+  patch '/items/:id' do
+  end
+  
+  delete '/items/:id' do
+  end
 
 end
