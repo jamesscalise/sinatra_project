aa1df8f93ca5b75323c539fc34fa2a7859441fc5
James Scalise <jamescscalise@gmail.com>
2019-12-17 16:28:59 +0000

    uncomment items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,34 +1,34 @@
 class ItemsController < App
   
   get '/items' do
-   # if logged_in?
-    #  @items = Item.all
+   if logged_in?
+      @items = Item.all
       erb :'items/items'
-   # else
-   #   redirect '/login'
-   # end
+    else
+      redirect '/login'
+   end
   end
   
   get '/items/new' do
-   # if logged_in?
+    if logged_in?
       erb :'items/new'
-   # else
-   #   redirect '/login'
-   # end
+    else
+      redirect '/login'
+    end
   end
   
   post '/items' do
-    # params[:name] != ""
-    #  @item = Item.create(content: params[:name], user: current_user)
-     # redirect "/items/#{@item.id}"
-    #else
-    #  redirect '/items/new'
-   # end
+     params[:name] != ""
+     @item = Item.create(content: params[:name], user: current_user)
+      redirect "/items/#{@item.id}"
+    else
+     redirect '/items/new'
+    end
   end
   
   get '/items/:id' do
-   # @item = Item.find(params[:id])
-     # erb :'items/show_item'
+    @item = Item.find(params[:id])
+      erb :'items/show_item'
   end
 
 
